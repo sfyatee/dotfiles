2b0e388eac0d6e93e29d03dadb14269009a1d8e0
Demian Garcia <dag@demians.org>
2025-02-26 20:16:56 -0800

    emacs: colorcolumn thing works
    this is a test this is a test this is a test this is a test this is
    a test. word wrap works

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -97,4 +97,5 @@
 (use-package vc-got :ensure t)
 
 ;; vc
+(add-hook 'log-edit-mode-hook (lambda () (setq fill-column 68) (auto-fill-mode 1)))
 (add-hook 'log-edit-mode-hook (lambda () (setq display-fill-column-indicator-column 69) (display-fill-column-indicator-mode 1)))
