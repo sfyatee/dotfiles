a6b798f6f8236882391deeebc53737dfef7c2827
Demian Garcia <dag@demians.org>
2025-04-09 15:51:54 -0700

    ...

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,7 +21,7 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.opt.cursorline = true
+vim.opt.cursorline = false
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.mousescroll = "ver:1,hor:6"
