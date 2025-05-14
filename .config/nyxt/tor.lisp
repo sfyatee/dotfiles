cfe7ac9e18cbfe4723cd0684af1805286bc5c55e
Demian Garcia <dag@demians.org>
2025-05-13 20:11:04 -0700

    nyxt tor

--- .config/nyxt/tor.lisp
+++ .config/nyxt/tor.lisp
@@ -1,5 +1,5 @@
-(define-configuration nyxt/mode/proxy:proxy-mode
-  ((nyxt/mode/proxy:proxy (make-instance 'proxy
-                                         :url (quri:uri "socks5://localhost:9050")
-                                         :allowlist '("localhost" "localhost:8080")
-                                         :proxied-downloads-p t))))
+(define-configuration :proxy-mode
+  ((:proxy (make-instance 'proxy
+                          :url (quri:uri "socks5://localhost:9050")
+                          :allowlist '("localhost" "localhost:8080")
+                          :proxied-downloads-p t))))
