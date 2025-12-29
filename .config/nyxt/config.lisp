726fcc899198d66f8dd8e9974b2abcb8a9444bb1
demian garcia <dag@demians.org>
2025-12-29 05:23:18 -0600

    .

--- .config/nyxt/config.lisp
+++ .config/nyxt/config.lisp
@@ -31,7 +31,7 @@ Why the variable? Because it's too much hassle copying it everywhere.")
 (define-configuration :style-mode ((glyph "ϕ")))
 (define-configuration :cruise-control-mode ((glyph "σ")))
 
-(defmethod format-status-load-status ((status status-buffer))
+(defmethod my-format-status-load-status ((status status-buffer))
   "A fancier load status."
   (spinneret:with-html-string
    (:span (if (and (current-buffer)
@@ -41,6 +41,76 @@ Why the variable? Because it's too much hassle copying it everywhere.")
                     (:loading "∞")
                     (:finished ""))
             ""))))
+
+(defmethod my-format-status-url ((status status-buffer))
+  (let* ((buffer (current-buffer (window status)))
+         (url-display (multiple-value-bind (aesthetic safe)
+                          (render-url (url buffer))
+                        (uiop:strcat
+                         (if safe
+                             (format nil "~a (~a)" safe aesthetic)
+                             ;; RFC 2068 says 255 bytes is recommended max, thats 32 characters
+                             ;; 62 is the average, so 32 should be ample for the nessecary info
+                             (str:prune 32 aesthetic :ellipsis "…"))
+                         (when (title buffer)
+                           (str:concat " — " (title buffer)))
+                         (when (find (url buffer) (remove buffer (buffer-list))
+                                     :test #'url-equal :key #'url)
+                           (format nil " (buffer ~a)" (id buffer)))))))
+    (spinneret:with-html-string
+      (:nbutton :buffer status :text url-display :title url-display
+        '(nyxt:set-url)))))
+
+(defun enabled-modes-string (buffer)
+  (when (modable-buffer-p buffer)
+    (format nil "~{~a~^~%~}" (mapcar #'princ-to-string (serapeum:filter #'enabled-p (modes buffer))))))
+
+(defmethod my-format-minions ((status status-buffer))
+  (let ((buffer (current-buffer (window status))))
+    (if (modable-buffer-p buffer)
+        (spinneret:with-html-string
+          (:nbutton
+            :buffer status
+            :text ";-"
+            :title (str:concat "Enabled modes: " (enabled-modes-string buffer))
+            '(nyxt:toggle-modes)))
+    "")))
+
+(defmethod my-format-modes ((status status-buffer))
+  (let ((buffer (current-buffer (window status))))
+    (if (modable-buffer-p buffer)
+      (str:concat "(" (enabled-modes-string buffer) ")")
+      "")))
+
+(defmethod format-status ((status status-buffer))
+  (let* ((buffer (current-buffer (window status)))
+         (buffer-count (1+ (or (position buffer
+                                         (sort (buffer-list) #'url-equal :key #'url))
+                               0))))
+    (spinneret:with-html-string
+      (:div :id "container"
+            ;; for looks, I should probably make this functional
+            (:div :id "vi-mode" "U:**-")
+            (:div :id "buffers"
+                  (format nil "[~a/~a]"
+                      buffer-count
+                      (length (buffer-list))))
+            ;;(:div :id "percentage"
+            ;;      (format nil "L~a"
+            ;;          (%percentage)))
+             (:div :id "url"
+                   (:raw
+                    (my-format-status-load-status status)
+                    (my-format-status-url status)))
+             (:div :id "tabs"
+                   (:raw
+                    (format-status-tabs status)))
+             (:div :id "minions"
+                   (:raw 
+                    (my-format-minions status)))
+             (:div :id "modes"
+                   (:raw
+                     (my-format-modes status)))))))
 
 (define-configuration :status-buffer
   ((height 36)
