e3d04db26587c2dd117bf6cb77b67c18f6a9580a
demian garcia <dag@demians.org>
2025-12-29 01:40:49 -0600

    .

--- .config/nyxt/config.lisp
+++ .config/nyxt/config.lisp
@@ -1,3 +1,17 @@
+(defvar *web-buffer-modes*
+  '(:emacs--mode
+    :blocker-mode :force-https-mode
+    :reduce-tracking-mode
+    :user-script-mode :bookmarklets-mode)
+  "The modes to enable in any web-buffer by default.
+Extension files (like dark-reader.lisp) are to append to this list.
+
+Why the variable? Because it's too much hassle copying it everywhere.")
+
+(define-configuration :web-buffer
+  "Basic modes setup for web-buffer."
+  ((default-modes `(,@*web-buffer-modes* ,@%slot-value%))))
+
 (define-configuration :force-https-mode ((glyph "ϕ")))
 (define-configuration :user-script-mode ((glyph "u")))
 (define-configuration :blocker-mode ((glyph "β")))
