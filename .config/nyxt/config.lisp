cdba6bdef028b09a934716014e988d6fba2d79fd
demian garcia <dag@demians.org>
2025-12-29 05:15:38 -0600

    .

--- .config/nyxt/config.lisp
+++ .config/nyxt/config.lisp
@@ -47,7 +47,7 @@ Why the variable? Because it's too much hassle copying it everywhere.")
    (style
     (theme:themed-css (theme *browser*)
       `(*
-        :font-family "monospace"
+        :font-family ,theme:monospace-font-family
         :font-size "11px")
       `(body
         :margin "9px"
