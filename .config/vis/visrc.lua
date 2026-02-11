7ff8943563a274ed639c50b9fe6f1999819c9b98
demian garcia <dag@demians.org>
2026-02-11 04:28:59 -0800

    .

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -1,8 +1,9 @@
 require("vis")
-require("vis-leah-open")
+--require("vis-leah-open")
 
 vis.events.subscribe(vis.events.INIT, function()
 	vis:command("set theme sfyatee")
+	vis.options = { autoindent = true }
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
