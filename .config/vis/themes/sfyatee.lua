d5019875cc2c7871ee5a5dc2a539494a75ddb5fb
demian garcia <dag@demians.org>
2026-02-11 04:25:44 -0800

    .

--- .config/vis/themes/sfyatee.lua
+++ .config/vis/themes/sfyatee.lua
@@ -1,4 +1,3 @@
 local lexers = vis.lexers
 
-lexers.STYLE_STATUS = 'back:#e6ffff'
-lexers.STYLE_STATUS_FOCUSED = 'back:#fffaf3,bold'
+lexers.STYLE_STATUS_FOCUSED = 'back:#fffaf3'
--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -4,3 +4,7 @@ require("vis-leah-open")
 vis.events.subscribe(vis.events.INIT, function()
 	vis:command("set theme sfyatee")
 end)
+
+vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+	vis:command('set syntax off')
+end)
