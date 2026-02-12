13367cbde2dc719bbd986e9e47d2a4bd26924a4a
demian garcia <dag@demians.org>
2026-02-12 01:53:32 -0800

    .

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -1,6 +1,17 @@
 require("vis")
 --require("vis-leah-open")
 
+local plug = (function() if not pcall(require, 'plugins/vis-plug') then
+ 	os.execute('git clone --quiet https://github.com/erf/vis-plug ' ..
+	 	(os.getenv('XDG_CONFIG_HOME') or os.getenv('HOME') .. '/.config')
+	 	.. '/vis/plugins/vis-plug')
+end return require('plugins/vis-plug') end)()
+
+-- configure plugins in an array of tables with git urls and options
+local plugins = {
+	{ 'fischerling/vis-lspc' },
+}
+
 vis.events.subscribe(vis.events.INIT, function()
 	vis:command("set theme sfyatee")
 	vis.options = { autoindent = true }
