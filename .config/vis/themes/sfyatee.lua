db33f7fc5de3b3725f5f00d613ea511b29d3bb17
demian garcia <dag@demians.org>
2026-04-17 14:53:55 -0700

    .

--- .config/vis/themes/sfyatee.lua
+++ .config/vis/themes/sfyatee.lua
@@ -1,3 +1,4 @@
 local lexers = vis.lexers
 
-lexers.STYLE_STATUS_FOCUSED = 'back:#fffaf3'
+lexers.STYLE_STATUS            = 'fore:#0c0a0e,back:#fffaf3'
+lexers.STYLE_STATUS_FOCUSED    = lexers.STYLE_STATUS .. ',bold'
