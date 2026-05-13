cc681241ac79c2e5dc57d7fd5248a460e17782cf
demian garcia <dag@demians.org>
2026-05-12 20:11:33 -0700

    .

--- .config/vis/themes/sfyatee.lua
+++ .config/vis/themes/sfyatee.lua
@@ -1,13 +1,14 @@
 local lexers = vis.lexers
 
 lexers.STYLE_COMMENT           = 'fore:#9893a5,italics'
-lexers.STYLE_FUNCTION          = 'fore:default'
+lexers.STYLE_FUNCTION          = ''
 lexers.STYLE_KEYWORD           = 'fore:white,bold'
 lexers.STYLE_NUMBER            = 'fore:white,underlined'
 lexers.STYLE_OPERATOR          = 'fore:white'
 lexers.STYLE_STRING            = 'fore:white,underlined'
 lexers.STYLE_PREPROCESSOR      = 'underlined'
 lexers.STYLE_TYPE              = 'fore:default,bold'
+lexers.STYLE_VARIABLE          = ''
 
 lexers.STYLE_STATUS            = 'fore:#0c0a0e,back:#fffaf3'
 lexers.STYLE_STATUS_FOCUSED    = lexers.STYLE_STATUS .. ',bold'
