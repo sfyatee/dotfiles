8b96f4a1b95589c09a320f8ebdc52a316c2aac11
demian garcia <dag@demians.org>
2026-05-12 19:35:36 -0700

    .

--- .config/vis/themes/sfyatee.lua
+++ .config/vis/themes/sfyatee.lua
@@ -1,4 +1,11 @@
 local lexers = vis.lexers
 
+--lexers.STYLE_CONSTANT          = 'fore:black,underlined'
+lexers.STYLE_KEYWORD           = 'fore:white,bold'
+lexers.STYLE_NUMBER            = 'fore:white,underlined'
+lexers.STYLE_OPERATOR          = 'fore:white'
+lexers.STYLE_STRING            = 'fore:white,underlined'
+lexers.STYLE_PREPROCESSOR      = 'underlined'
+
 lexers.STYLE_STATUS            = 'fore:#0c0a0e,back:#fffaf3'
 lexers.STYLE_STATUS_FOCUSED    = lexers.STYLE_STATUS .. ',bold'
--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -1,25 +1,10 @@
 require("vis")
 --require("vis-leah-open")
 
-local plug = (function() if not pcall(require, 'plugins/vis-plug') then
- 	os.execute('git clone --quiet https://github.com/erf/vis-plug ' ..
-	 	(os.getenv('XDG_CONFIG_HOME') or os.getenv('HOME') .. '/.config')
-	 	.. '/vis/plugins/vis-plug')
-end return require('plugins/vis-plug') end)()
-
--- configure plugins in an array of tables with git urls and options
-local plugins = {
-	{ 'https://repo.or.cz/vis-surround.git' },
-}
-
--- require and optionally install plugins on init
-plug.init(plugins, true)
-
 vis.events.subscribe(vis.events.INIT, function()
 	vis:command("set theme sfyatee")
 	vis.options = { autoindent = true }
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
-	vis:command('set syntax off')
 end)
