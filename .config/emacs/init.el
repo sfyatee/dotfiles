de7169c22c1ae62d475f2bbbb80344708f3460d2
demian garcia <dag@demians.org>
2026-07-04 19:49:18 -0700

    ...

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -44,17 +44,10 @@
   :ensure t
   :demand t
   :config
-  ;; These are the default values.
   (setq doric-themes-to-toggle '(doric-light doric-dark))
   (setq doric-themes-to-rotate doric-themes-collection)
 
-  (doric-themes-select 'doric-obsidian)
-
-  ;; ;; To load a random theme instead, use something like one of these:
-  ;;
-  ;; (doric-themes-load-random)
-  ;; (doric-themes-load-random 'light)
-  ;; (doric-themes-load-random 'dark)
+  (doric-themes-select 'doric-dark)
 
   ;; ;; For optimal results, also define your preferred font family (or use my `fontaine' package):
   ;;
